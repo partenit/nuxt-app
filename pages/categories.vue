bed91ae4f430b968bf2239f337618afc0cf61983
Sergey <sergey.h@gmail.com>
2022-11-24 15:26:01 +0200

    добавлена главная страница - с продуктами добавлена страница категорий

---
+++ pages/categories.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="px-6 py-3 min-w-600 overflow-hidden overflow-x-auto align-middle sm:rounded-md">
+    <table class="min-w-full border divide-y divide-gray-200">
+      <thead>
+      <tr>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">ID</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Название</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Описание</span>
+        </th>
+      </tr>
+      </thead>
+
+      <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+      <template v-for="item in items" :key="item.id">
+        <tr class="bg-white">
+          <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+            {{ item.id }}
+          </td>
+          <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+            {{ item.name }}
+          </td>
+          <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+            {{ item.description }}
+          </td>
+        </tr>
+      </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+  import {useFetch} from "nuxt/app";
+
+  const response = await useFetch('https://test-shop.estater.biz/api/v1/categories')
+  const items = response.data._rawValue.data
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="px-6 py-3 min-w-600 overflow-hidden overflow-x-auto align-middle sm:rounded-md">
+    <table class="min-w-full border divide-y divide-gray-200">
+      <thead>
+      <tr>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase"></span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Название</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Описание</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Цена</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Код</span>
+        </th>
+        <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Категория</span>
+        </th>
+      </tr>
+      </thead>
+
+      <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+        <template v-for="item in items" :key="item.id">
+          <tr class="bg-white">
+            <td class="text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              <img src="https://picsum.photos/100" style="width:100px;"/>
+            </td>
+            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              {{ item.name }}
+            </td>
+            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              {{ item.description }}
+            </td>
+            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              {{ item.price }}
+            </td>
+            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              {{ item.code }}
+            </td>
+            <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+              {{ item.category.name }}
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+  import {useFetch} from "nuxt/app";
+
+  const response = await useFetch('https://test-shop.estater.biz/api/v1/products')
+  const items = response.data._rawValue.data
+
+</script>
