4ce4934562e368ff399b82ed1ec22141ec640e1c
Sergey <sergey.h@gmail.com>
2022-11-24 14:36:49 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Laravel Api with Nuxt 3 Fetch Api Data</h1>
+    <ul v-for="category in categories" :key="category.id">
+      <li>Id: {{ category.id }}</li>
+      <li>Name: {{ category.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {useFetch} from "nuxt/app";
+
+const response = await useFetch('https://test-shop.estater.biz/api/v1/categories')
+const categories = response.data._rawValue.data
+console.log(categories)
+
+</script>
+
